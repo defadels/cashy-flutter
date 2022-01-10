3f7a7896cb5acd21718c099f0081b135b3d56a3e
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-01-10 17:30:24 +0700

    Failed to load image

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Save Your Treasure'),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Cashy"),
+          backgroundColor: Colors.red,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.mail),
+              color: Colors.white,  
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: Image(
+           image: AssetImage('assets/images/gambar.png'),
+           height: 200,
+        ),
+        )));
   }
 }
 
